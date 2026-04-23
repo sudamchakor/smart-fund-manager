5acea3388067857eb154448961487e727a93ea41
sudam chakor <chakorsudam@gmail.com>
2026-04-23 11:24:28 +0530

    Fixed fab button z index issue for future goals

--- src/features/profile/tabs/FutureGoalsTab.jsx
+++ src/features/profile/tabs/FutureGoalsTab.jsx
@@ -753,7 +753,7 @@ export default function FutureGoalsTab({ goalToEditId }) {
               position: "fixed",
               bottom: 16,
               right: 16,
-              zIndex: 1000, // Added zIndex
+              zIndex: 99999, // Added zIndex
             }}
             onClick={handleOpenFabMenu}
             disabled={currentSurplus < 0}
