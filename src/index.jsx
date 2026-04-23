a9a2b4b069c9dbb05c0fb393dd6506078c5a9869
sudam chakor <chakorsudam@gmail.com>
2026-04-23 12:04:44 +0530

    updated base router

--- src/index.jsx
+++ src/index.jsx
@@ -3,13 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./styles/global.css"; // Updated import
 import "./styles/theme.css"; // New import
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom"; // Changed to HashRouter
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/emiCalculator">
+    <HashRouter> {/* Changed to HashRouter and removed basename */}
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
